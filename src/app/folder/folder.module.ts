ca09ab99f2249584f3212b1c3fbb06a5159ad5de
siracideshiphop@gmail.com <siracideshiphop@gmail.com>
2026-06-22 02:12:09 -0300

    Evaluacion 2: Formulario Reactivo e Interaccion Terminada - Johan Gonzalez

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { IonicModule } from '@ionic/angular';
-
 import { FolderPageRoutingModule } from './folder-routing.module';
-
-import { FolderPage } from './folder.page';
+import { FolderPage } from './folder.page'; 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule, 
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    FolderPage 
   ],
-  declarations: [FolderPage]
+  declarations: [] 
 })
 export class FolderPageModule {}
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  standalone: false,
+  standalone: true, 
+  imports: [CommonModule, IonicModule, ReactiveFormsModule] 
 })
 export class FolderPage implements OnInit {
   public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  public contactoForm!: FormGroup;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id') || '';
+
+    this.contactoForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onEnviarContacto() {
+    if (this.contactoForm.valid) {
+      console.log('Datos de Johan validados listos para procesamiento:', this.contactoForm.value);
+      alert('¡Formulario enviado con éxito de forma segura por Johan Gonzalez!');
+      this.contactoForm.reset();
+    } else {
+      alert('Error: Verifique las entradas del formulario.');
+    }
   }
 }
